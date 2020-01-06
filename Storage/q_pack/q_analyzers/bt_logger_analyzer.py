b43db95caffc06e8993059b82c09ab2a48bb100b
Saeed Rahman <saeed349>
2020-01-06 10:19:31 -0500

    Update: Logger analyzer datetime column changed

--- Storage/q_pack/q_analyzers/bt_logger_analyzer.py
+++ Storage/q_pack/q_analyzers/bt_logger_analyzer.py
@@ -20,14 +20,13 @@ class logger_analyzer(bt.Analyzer):
                 ml_dict={}
                 data_size=len(d)
                 ml_dict["security"]=[d._name]*data_size
-                ml_dict["datetime"]=[self.data.num2date(x) for x in self.data.datetime.get(size=data_size)]
+                ml_dict["datetime"]=[self.data.num2date(x) for x in d.datetime.get(size=data_size)]#self.data
                 ml_dict["close"]=d.get(size=data_size)
                 num_of_indicators=int(len(self.strategy.getindicators())/len(self.strategy.datas))
                 for j in range(num_of_indicators):
                     ml_dict[self.strategy.getindicators()[j*num_of_sec+i].aliased]=self.strategy.getindicators()[j*num_of_sec+i].get(size=data_size) # tested for 3 conditions , indicators >,<,= securities
                 ml_list.append(pd.DataFrame(ml_dict))  
-            ml_df = pd.concat(ml_list)
-
+            ml_df = pd.concat(ml_list,axis=0)
             s3 = boto3.client('s3',endpoint_url="http://minio-image:9000",aws_access_key_id="minio-image",aws_secret_access_key="minio-image-pass")
             Bucket="model-support-files"
             Key=str(self.strategy.db_run_id)+"_ml_log.csv"
